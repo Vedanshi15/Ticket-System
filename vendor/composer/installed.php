86a584c05bf0a028dafe4949b8d209d359515bd9
Vedanshi15 <patelvedu15@gmail.com>
2021-09-08 21:13:04 -0400

    Added Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '93c17f79d6d37a7b4c8ef5ba30b5d8574688a2fd',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '93c17f79d6d37a7b4c8ef5ba30b5d8574688a2fd',
+            'dev_requirement' => false,
+        ),
+    ),
+);
